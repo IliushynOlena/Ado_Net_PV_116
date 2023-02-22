37c136fcc11bd0ab17653d1d931dde24d6998988
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-02-22 19:26:39 +0200

    Move data seeding to the extencions in a separate file

--- 07_EF_example/AirplaneDbContext.cs
+++ 07_EF_example/AirplaneDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using _07_EF_example.Entities;
+using _07_EF_example.Helpers;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Data;
@@ -39,45 +40,10 @@ namespace _07_EF_example
         {
             base.OnModelCreating(modelBuilder);
             //Initializer - Seeder
-            modelBuilder.Entity<Airplane>().HasData(new Airplane[]
-            {
-                new Airplane()
-                {
-                    Id = 1,
-                    Model = "Boeing 747",
-                    MaxPassangers = 1200
-                },
-                new Airplane()
-                {
-                    Id = 2,
-                    Model = "Boeing 425",
-                    MaxPassangers = 1300
-                }
-            });
+            modelBuilder.SeedAirplanes();
+            modelBuilder.SeedFligths();
 
-            modelBuilder.Entity<Flight>().HasData(new Flight[]
-            {
-                new Flight()
-                {
-                    Number = 1,
-                    AirplaneId = 1,
-                    DepartureCity = "Kyiv",
-                    ArrivelCity = "Lviv",
-                    DepartureTime = new DateTime(2023,3,3),
-                    ArrivelTime = new DateTime(2023,3,3)
-
-                },
-                new Flight()
-                {
-                    Number = 2,
-                    AirplaneId = 2,
-                    DepartureCity = "Warsaw",
-                    ArrivelCity = "Lviv",
-                    DepartureTime = new DateTime(2023,3,3),
-                    ArrivelTime = new DateTime(2023,3,3)
-
-                }
-            });
+        
 
             //Fluent API configuration
             modelBuilder.Entity<Airplane>().Property(a => a.Model)
---
+++ 07_EF_example/Helpers/DbInitializer.cs
@@ -0,0 +1,60 @@
+﻿using _07_EF_example.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_EF_example.Helpers
+{
+    internal static class DbInitializer
+    {
+        public static void SeedAirplanes(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Airplane>().HasData(new Airplane[]
+         {
+                new Airplane()
+                {
+                    Id = 1,
+                    Model = "Boeing 747",
+                    MaxPassangers = 1200
+                },
+                new Airplane()
+                {
+                    Id = 2,
+                    Model = "Boeing 425",
+                    MaxPassangers = 1300
+                }
+         });
+
+        }
+        public static void SeedFligths(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Flight>().HasData(new Flight[]
+        {
+                new Flight()
+                {
+                    Number = 1,
+                    AirplaneId = 1,
+                    DepartureCity = "Kyiv",
+                    ArrivelCity = "Lviv",
+                    DepartureTime = new DateTime(2023,3,3),
+                    ArrivelTime = new DateTime(2023,3,3)
+
+                },
+                new Flight()
+                {
+                    Number = 2,
+                    AirplaneId = 2,
+                    DepartureCity = "Warsaw",
+                    ArrivelCity = "Lviv",
+                    DepartureTime = new DateTime(2023,3,3),
+                    ArrivelTime = new DateTime(2023,3,3)
+
+                }
+        });
+        }
+    }
+}
